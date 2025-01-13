2641daa22e5ea735983437321423f8187c82f450
Mike Oliveira <mikeoliveira@outlook.com>
2025-01-12 22:40:49 -0300

    feat(variaveis-estilo): adicionado variaveis de estilo para o projeto

--- apps/api-rick-morty/src/app/app.component.ts
+++ apps/api-rick-morty/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { LayoutCustomLabelModule } from '@api-collection-nx/ui/layout-custom-label';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, LayoutCustomLabelModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
